567150d56af239331c367af225f708329b8f395f
Juliano Dasilva <juliano.dasilva@target.com>
2017-04-05 12:30:08 -0500

    added props

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const searchBar = () => {
-  return <h1>Juliano</h1>;
-};
+// functional component
 
-export default searchBar;
+// const SearchBar = () => {
+//   return <input />;
+// };
+
+// Class based component: must have a render function
+
+class SearchBar extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { term: '' };
+    }
+
+    render () {
+      return (
+        <div>
+          <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+        </div>
+      )
+    }
+
+    // onInputChange(event) {
+    // }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-import searchBar from './components/search_bar';
-
-const API_KEY = 'AIzaSyAqudaTcnl2w2vt3CxskfQHYcQjiDvA73c';
+const API_KEY = 'AIzaSyBfXK51W99oeqZCnyBRUDP7ZuoUBpSrL6s';
 
 // Create a component. This component should produce HTML. The component here is an object (or class)
 // so, you can't produce HTML from a class. You need to instantiate it.
 
-const App = () => {
-  return (
-    <div>
-      <searchBar />
-    </div>
-  );
+// Functional component
+// const App = () => {
+//   return (
+//     <div>
+//       <SearchBar />
+//     </div>
+//   );
+// }
+
+// Switched from a functional component to class-based component
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState = ({ videos });
+      console.log(videos);
+      // this.setState = ({videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+
 }
 
 // Take this component's HTML and put it on the page (DOM) using ReactDOM. You need to pass an instante
