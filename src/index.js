8f4fdd6f1e7eea87c803e9767160a7a0fadb9d25
Juliano Dasilva <juliano.dasilva@target.com>
2017-07-10 16:15:10 -0500

    created VideoListItem component with props to retrieve data from videos and append to VideoList. List item keys

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 // Create a new component. This component should produce HTML.
 
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={ this.state.videos }/>
       </div>
     )
   }
