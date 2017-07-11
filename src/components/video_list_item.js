2a7c1c71facd13c40f3217a0eeddc8af6ca02fd6
Juliano Dasilva <juliano.dasilva@target.com>
2017-07-10 21:33:43 -0500

    added video_detail and loaded a video

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+const VideoListItem = ({video}) => {
+  // using ES6 goodies {video} as props
+  // accessing props from video_list.js. see VideoListItem in video_list.js
+  // const video = props.video;
+  // console.log(video);
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src="{imageUrl}" />
+        </div>
+        <div className="media-right">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
+        </div>
+      </div>
+
+    </li>
+  );
 }
 
 export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar'
-import VideoList from './components/video_list'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 // Create a new component. This component should produce HTML.
 
@@ -23,7 +24,8 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={ this.state.videos }/>
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos}/>
       </div>
     )
   }
